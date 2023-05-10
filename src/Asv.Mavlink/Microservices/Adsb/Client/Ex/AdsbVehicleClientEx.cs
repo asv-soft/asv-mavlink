2166603246a38f59538d2e8af5b4595287cc02bd
Aleksandr Burlakov <u01@asv.me>
2023-05-10 15:18:21 +0300

    ADSB Vehicle extended client implemented

---
+++ src/Asv.Mavlink/Microservices/Adsb/Client/Ex/AdsbVehicleClientEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Asv.Common;
+using Asv.Mavlink.V2.Common;
+using DynamicData;
+
+namespace Asv.Mavlink;
+
+public class AdsbVehicleClientEx : DisposableOnceWithCancel, IAdsbVehicleClientEx
+{
+    private readonly SourceCache<AdsbVehiclePayload, uint> _targetSource;
+    private readonly ReadOnlyObservableCollection<AdsbVehiclePayload> _targets;
+
+    public AdsbVehicleClientEx(IAdsbVehicleClient client)
+    {
+        if (client == null) throw new ArgumentNullException(nameof(client));
+
+        _targetSource = new SourceCache<AdsbVehiclePayload, uint>(_ => _.IcaoAddress).DisposeItWith(Disposable);
+        _targetSource.Connect().Bind(out _targets).Subscribe().DisposeItWith(Disposable);
+
+        client.Target.Subscribe(_targetSource.AddOrUpdate).DisposeItWith(Disposable);
+    }
+
+    public ReadOnlyObservableCollection<AdsbVehiclePayload> Targets => _targets;
+}
---
+++ src/Asv.Mavlink/Microservices/Adsb/Client/Ex/IAdsbVehicleClientEx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Asv.Mavlink.V2.Common;
+using DynamicData;
+
+namespace Asv.Mavlink;
+
+public interface IAdsbVehicleClientEx : IDisposable
+{
+    ReadOnlyObservableCollection<AdsbVehiclePayload> Targets { get; }
+}
