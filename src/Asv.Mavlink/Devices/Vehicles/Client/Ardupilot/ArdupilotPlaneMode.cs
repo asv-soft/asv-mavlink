20d6d83c074885939b47fec2b5ad7a1e24d4b1dd
Aleksandr Burlakov <u01@asv.me>
2023-06-26 14:41:18 +0300

    feat: add mode description localization
    Asana: https://app.asana.com/0/1203851531040615/1204903646366526/f

--- src/Asv.Mavlink/Devices/Vehicles/Client/Ardupilot/ArdupilotPlaneMode.cs
+++ src/Asv.Mavlink/Devices/Vehicles/Client/Ardupilot/ArdupilotPlaneMode.cs
@@ -55,79 +55,79 @@ public class ArdupilotPlaneMode:VehicleMode
     /// <summary>
     /// Manual control surface movement, passthrough
     /// </summary>
-    public static ArdupilotPlaneMode Manual      = new("Manual", "Manual", PlaneMode.PlaneModeManual);
+    public static ArdupilotPlaneMode Manual      = new("Manual", RS.ArdupilotPlaneMode_Manual_Description, PlaneMode.PlaneModeManual);
     /// <summary>
     /// Gently turns aircraft
     /// </summary>
-    public static ArdupilotPlaneMode Circle      = new("Circle", "Circle", PlaneMode.PlaneModeCircle);
+    public static ArdupilotPlaneMode Circle      = new("Circle", RS.ArdupilotPlaneMode_Circle_Description, PlaneMode.PlaneModeCircle);
     /// <summary>
     /// Wing-leveling on stick release
     /// </summary>
-    public static ArdupilotPlaneMode Stabilize   = new("Stabilize", "Stabilize", PlaneMode.PlaneModeStabilize);
+    public static ArdupilotPlaneMode Stabilize   = new("Stabilize", RS.ArdupilotPlaneMode_Stabilize_Description, PlaneMode.PlaneModeStabilize);
     /// <summary>
     /// Manual control up to roll and pitch limits
     /// </summary>
-    public static ArdupilotPlaneMode Training    = new("Training", "Training", PlaneMode.PlaneModeTraining);
+    public static ArdupilotPlaneMode Training    = new("Training", RS.ArdupilotPlaneMode_Training_Description, PlaneMode.PlaneModeTraining);
     /// <summary>
     /// Rate controlled mode with no attitude limits
     /// </summary>
-    public static ArdupilotPlaneMode Acro        = new("Acro", "Acro", PlaneMode.PlaneModeAcro);
+    public static ArdupilotPlaneMode Acro        = new("Acro", RS.ArdupilotPlaneMode_Acro_Description, PlaneMode.PlaneModeAcro);
     /// <summary>
     /// Roll and pitch follow stick input, up to set limits
     /// </summary>
-    public static ArdupilotPlaneMode FlyByWireA  = new("Fly by wire A", "Fly by wire A", PlaneMode.PlaneModeFlyByWireA);
+    public static ArdupilotPlaneMode FlyByWireA  = new("Fly by wire A", RS.ArdupilotPlaneMode_FlyByWireA_Description, PlaneMode.PlaneModeFlyByWireA);
     /// <summary>
     /// Like FBWA, but with automatic height and speed control
     /// </summary>
-    public static ArdupilotPlaneMode FlyByWireB  = new("Fly by wire B", "Fly by wire B", PlaneMode.PlaneModeFlyByWireB);
+    public static ArdupilotPlaneMode FlyByWireB  = new("Fly by wire B", RS.ArdupilotPlaneMode_FlyByWireB_Description, PlaneMode.PlaneModeFlyByWireB);
     /// <summary>
     /// Like FBWB, but with ground course tracking and terrain following
     /// </summary>
-    public static ArdupilotPlaneMode Cruise      = new("Cruise", "Cruise", PlaneMode.PlaneModeCruise);
+    public static ArdupilotPlaneMode Cruise      = new("Cruise", RS.ArdupilotPlaneMode_Cruise_Description, PlaneMode.PlaneModeCruise);
     /// <summary>
     /// Like FBWA, but learns attitude tuning while flying
     /// </summary>
-    public static ArdupilotPlaneMode Autotune    = new("Autotune", "Autotune", PlaneMode.PlaneModeAutotune);
+    public static ArdupilotPlaneMode Autotune    = new("Autotune", RS.ArdupilotPlaneMode_Autotune_Description, PlaneMode.PlaneModeAutotune);
     /// <summary>
     /// Follows Mission
     /// </summary>
-    public static ArdupilotPlaneMode Auto        = new("Auto", "Auto", PlaneMode.PlaneModeAuto);
+    public static ArdupilotPlaneMode Auto        = new("Auto", RS.ArdupilotPlaneMode_Auto_Description, PlaneMode.PlaneModeAuto);
     /// <summary>
     /// Returns to and circles home or rally point
     /// </summary>
-    public static ArdupilotPlaneMode Rtl         = new("RTL", "RTL", PlaneMode.PlaneModeRtl);
+    public static ArdupilotPlaneMode Rtl         = new("RTL", RS.ArdupilotPlaneMode_Rtl_Description, PlaneMode.PlaneModeRtl);
     /// <summary>
     /// Circles point where mode switched
     /// </summary>
-    public static ArdupilotPlaneMode Loiter      = new("Loiter", "Loiter", PlaneMode.PlaneModeLoiter);
+    public static ArdupilotPlaneMode Loiter      = new("Loiter", RS.ArdupilotPlaneMode_Loiter_Description, PlaneMode.PlaneModeLoiter);
     /// <summary>
     /// Circles user defined point from GCS
     /// </summary>
-    public static ArdupilotPlaneMode Guided      = new("Guided", "Guided", PlaneMode.PlaneModeGuided);
+    public static ArdupilotPlaneMode Guided      = new("Guided", RS.ArdupilotPlaneMode_Guided_Description, PlaneMode.PlaneModeGuided);
     /// <summary>
     ///  Allows you to fly your vehicle manually, but self-levels the roll and pitch axis
     /// </summary>
-    public static ArdupilotPlaneMode Qstabilize  = new("Quad Stabilize", "Qstabilize", PlaneMode.PlaneModeQstabilize);
+    public static ArdupilotPlaneMode Qstabilize  = new("Quad Stabilize", RS.ArdupilotPlaneMode_Qstabilize_Description, PlaneMode.PlaneModeQstabilize);
     /// <summary>
     /// QuadPlane maintains a consistent altitude while allowing roll, pitch, and yaw to be controlled normally
     /// </summary>
-    public static ArdupilotPlaneMode Qhover      = new("Quad Hover", "Qhover", PlaneMode.PlaneModeQhover);
+    public static ArdupilotPlaneMode Qhover      = new("Quad Hover", RS.ArdupilotPlaneMode_Qhover_Description, PlaneMode.PlaneModeQhover);
     /// <summary>
     /// Automatically attempts to maintain the current location, heading and altitude
     /// </summary>
-    public static ArdupilotPlaneMode Qloiter     = new("Quad Loiter", "Qloiter", PlaneMode.PlaneModeQloiter);
+    public static ArdupilotPlaneMode Qloiter     = new("Quad Loiter", RS.ArdupilotPlaneMode_Qloiter_Description, PlaneMode.PlaneModeQloiter);
     /// <summary>
     /// Attempts to bring the QuadPlane straight down at the position the vehicle is located when the mode is entered
     /// </summary>
-    public static ArdupilotPlaneMode Qland       = new("Quad Land", "Qland", PlaneMode.PlaneModeQland);
+    public static ArdupilotPlaneMode Qland       = new("Quad Land", RS.ArdupilotPlaneMode_Qland_Description, PlaneMode.PlaneModeQland);
     /// <summary>
     /// Navigates QuadPlane from its current position to hover above the home position and then land
     /// </summary>
-    public static ArdupilotPlaneMode Qrtl        = new("Quad RTL", "Qrtl", PlaneMode.PlaneModeQrtl);
+    public static ArdupilotPlaneMode Qrtl        = new("Quad RTL", RS.ArdupilotPlaneMode_Qrtl_Description, PlaneMode.PlaneModeQrtl);
     /// <summary>
     /// Automated pitch and bank procedure to improve control loops
     /// </summary>
-    public static ArdupilotPlaneMode Qautotune   = new("Qautotune", "Qautotune", PlaneMode.PlaneModeQautotune);
+    public static ArdupilotPlaneMode Qautotune   = new("Qautotune", RS.ArdupilotPlaneMode_Qautotune_Description, PlaneMode.PlaneModeQautotune);
    
     
     #endregion
