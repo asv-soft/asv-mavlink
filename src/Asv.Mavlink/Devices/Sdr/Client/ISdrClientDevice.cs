b40167de1f817aa46f263ffcab7440ef1dc73ac6
Alexey <248997@gmail.com>
2023-10-03 18:26:03 +0500

    feat(sdr): make ISdrClientDevice inherit IClientDevice

--- src/Asv.Mavlink/Devices/Sdr/Client/ISdrClientDevice.cs
+++ src/Asv.Mavlink/Devices/Sdr/Client/ISdrClientDevice.cs
@@ -1,9 +1,8 @@
 namespace Asv.Mavlink;
 
-public interface ISdrClientDevice
+public interface ISdrClientDevice:IClientDevice
 {
     IAsvSdrClientEx Sdr { get; }
-    IHeartbeatClient Heartbeat { get; }
     ICommandClient Command { get; }
     IMissionClientEx Missions { get; }
     IParamsClientEx Params { get; }
