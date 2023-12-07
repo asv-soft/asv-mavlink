23ff37914462365d653ed4769d63403a08bc3d0a
u01 <u01@asv.me>
2023-12-07 19:22:55 +0300

    feat: refactor mission items methods for better flexibility and control
    The existing AddNavMissionItem and AddRoiMissionItem methods have been removed and replaced by newly created methods in IMissionClientEx class. The new methods now include additional parameters allowing to define more attributes of the mission item thus providing richer control when creating mission items. These changes were made to address the need for better customization and control over mission items.

--- src/Asv.Mavlink/Microservices/Missions/Client/Ex/IMissionClientEx.cs
+++ src/Asv.Mavlink/Microservices/Missions/Client/Ex/IMissionClientEx.cs
@@ -41,38 +41,6 @@ public static class MissionClientExHelper
         item.Param4.OnNext(0);
         return item;
     }
-    
-    public static MissionItem AddNavMissionItem(this IMissionClientEx vehicle, GeoPoint point)
-    {
-        var item = vehicle.Create();
-        item.Location.OnNext(point);
-        item.AutoContinue.OnNext(true);
-        item.Command.OnNext(MavCmd.MavCmdNavWaypoint);
-        item.Current.OnNext(false);
-        item.Frame.OnNext(MavFrame.MavFrameGlobalInt);
-        item.MissionType.OnNext(MavMissionType.MavMissionTypeMission);
-        item.Param1.OnNext(0);
-        item.Param2.OnNext(0);
-        item.Param3.OnNext(0);
-        item.Param4.OnNext(0);
-        return item;
-    }
-    
-    public static MissionItem AddRoiMissionItem(this IMissionClientEx vehicle, GeoPoint point)
-    {
-        var item = vehicle.Create();
-        item.Location.OnNext(point);
-        item.AutoContinue.OnNext(true);
-        item.Command.OnNext(MavCmd.MavCmdDoSetRoiLocation);
-        item.Current.OnNext(false);
-        item.Frame.OnNext(MavFrame.MavFrameGlobalInt);
-        item.MissionType.OnNext(MavMissionType.MavMissionTypeMission);
-        item.Param1.OnNext(0);
-        item.Param2.OnNext(0);
-        item.Param3.OnNext(0);
-        item.Param4.OnNext(0);
-        return item;
-    }
 
     /// <summary>
     /// Change speed and/or throttle set points. The value persists until it is overridden or there is a mode change
@@ -93,5 +61,81 @@ public static class MissionClientExHelper
         item.Param2.OnNext(speed);
         item.Param3.OnNext(throttle);
         return item;
+    }
+    
+    /// <summary>
+    /// Adds navigation waypoint to MissionClients collection
+    /// </summary>
+    /// <param name="vehicle">vehicle client</param>
+    /// <param name="point">item location</param>
+    /// <param name="holdTime">time in seconds</param>
+    /// <param name="acceptRadius">radius in meters</param>
+    /// <param name="passRadius">radius in meters</param>
+    /// <param name="yawAngle">angle in degrees</param>
+    /// <returns>MissionItem creation result</returns>
+    public static MissionItem AddNavMissionItem(this IMissionClientEx vehicle, GeoPoint point, 
+        float holdTime = 0, float acceptRadius = 0, float passRadius = 0, float yawAngle = float.NaN)
+    {
+        var missionItem = vehicle.Create();
+        missionItem.Location.OnNext(point);
+        missionItem.AutoContinue.OnNext(true);
+        missionItem.Command.OnNext(MavCmd.MavCmdNavWaypoint);
+        missionItem.Current.OnNext(false);
+        missionItem.Frame.OnNext(MavFrame.MavFrameGlobalInt);
+        missionItem.MissionType.OnNext(MavMissionType.MavMissionTypeMission);
+        missionItem.Param1.OnNext(holdTime);
+        missionItem.Param2.OnNext(acceptRadius);
+        missionItem.Param3.OnNext(passRadius);
+        missionItem.Param4.OnNext(yawAngle);
+        return missionItem;
+    }
+    
+    public static MissionItem AddTakeOffMissionItem(this IMissionClientEx vehicle, GeoPoint point, float pitch = 0, float yawAngle = float.NaN)
+    {
+        var missionItem = vehicle.Create();
+        missionItem.Location.OnNext(point);
+        missionItem.AutoContinue.OnNext(true);
+        missionItem.Command.OnNext(MavCmd.MavCmdNavTakeoff);
+        missionItem.Current.OnNext(false);
+        missionItem.Frame.OnNext(MavFrame.MavFrameGlobalInt);
+        missionItem.MissionType.OnNext(MavMissionType.MavMissionTypeMission);
+        missionItem.Param1.OnNext(pitch);
+        missionItem.Param2.OnNext(0.0f);
+        missionItem.Param3.OnNext(0.0f);
+        missionItem.Param4.OnNext(yawAngle);
+        return missionItem;
+    }
+
+    public static MissionItem AddLandMissionItem(this IMissionClientEx vehicle, GeoPoint point, float abortAltitude = 0,
+        PrecisionLandMode landMode = PrecisionLandMode.PrecisionLandModeDisabled, float yawAngle = float.NaN)
+    {
+        var missionItem = vehicle.Create();
+        missionItem.Location.OnNext(point);
+        missionItem.AutoContinue.OnNext(true);
+        missionItem.Command.OnNext(MavCmd.MavCmdNavLand);
+        missionItem.Current.OnNext(false);
+        missionItem.Frame.OnNext(MavFrame.MavFrameGlobalInt);
+        missionItem.MissionType.OnNext(MavMissionType.MavMissionTypeMission);
+        missionItem.Param1.OnNext(abortAltitude);
+        missionItem.Param2.OnNext((float)landMode);
+        missionItem.Param3.OnNext(0.0f);
+        missionItem.Param4.OnNext(yawAngle);
+        return missionItem;
+    }
+    
+    public static MissionItem AddRoiMissionItem(this IMissionClientEx vehicle, GeoPoint point, MavRoi roiMode = MavRoi.MavRoiLocation, float wpIndex = 0, float roiIndex = 0)
+    {
+        var missionItem = vehicle.Create();
+        missionItem.Location.OnNext(point);
+        missionItem.AutoContinue.OnNext(true);
+        missionItem.Command.OnNext(MavCmd.MavCmdDoSetRoi);
+        missionItem.Current.OnNext(false);
+        missionItem.Frame.OnNext(MavFrame.MavFrameGlobalInt);
+        missionItem.MissionType.OnNext(MavMissionType.MavMissionTypeMission);
+        missionItem.Param1.OnNext((float)roiMode);
+        missionItem.Param2.OnNext(wpIndex);
+        missionItem.Param3.OnNext(roiIndex);
+        missionItem.Param4.OnNext(0.0f);
+        return missionItem;
     }
 }
