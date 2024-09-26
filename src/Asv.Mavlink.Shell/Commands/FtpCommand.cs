5d2372919f3e998fb89452024558bd97d8ad740e
Maxim <u04@asv.me>
2024-09-26 14:36:25 +0300

    feat(ftp): add base functional for ftp browser command
    asana: https://app.asana.com/0/1203851531040615/1208388559101531/f

--- src/Asv.Mavlink.Shell/Commands/FtpCommand.cs
+++ src/Asv.Mavlink.Shell/Commands/FtpCommand.cs
@@ -1,24 +1,30 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Asv.Common;
 using Asv.IO;
+using DynamicData;
 using ManyConsole;
+using Spectre.Console;
 
 namespace Asv.Mavlink.Shell;
 
-public class FtpCommand:ConsoleCommand
+public class FtpCommand : ConsoleCommand
 {
-    private string _connectionString = "tcp://127.0.0.1:7343";
+    private string _connectionString = "tcp://127.0.0.1:5762";
     private readonly CancellationTokenSource _cancel = new CancellationTokenSource();
+    private ReadOnlyObservableCollection<FtpEntry> _tree;
 
     public FtpCommand()
     {
         IsCommand("ftp", "FTP browser");
         HasOption("cs=", $"Connection string. Default '{_connectionString}'", _ => _connectionString = _);
     }
-    
+
     public override int Run(string[] remainingArguments)
     {
         RunAsync().Wait();
@@ -32,24 +38,34 @@ public class FtpCommand:ConsoleCommand
         using var conn = MavlinkV2Connection.Create(port);
         var identity = new MavlinkClientIdentity(255, 255, 1, 1);
         var seq = new PacketSequenceCalculator();
-        using var ftpClient = new FtpClient(new MavlinkFtpClientConfig(), conn,identity,seq,TimeProvider.System);
+        using var ftpClient = new FtpClient(new MavlinkFtpClientConfig(), conn, identity, seq, TimeProvider.System);
         var ftpEx = new FtpClientEx(ftpClient);
         try
         {
-            /*
+            var tree = new List<Node<IFtpEntry, string>>();
             await ftpEx.Refresh("/");
             await ftpEx.Refresh("@SYS");
-            */
-            await using var mem = File.OpenWrite("test.txt");
-            await ftpEx.BurstDownloadFile("@PARAM/param.pck?withdefaults=1", mem, new CallbackProgress<double>(Console.WriteLine) );
-            //await ftpEx.BurstDownloadFile("/terrain/test.txt", mem, new CallbackProgress<double>(Console.WriteLine) );
+            ftpEx.Entries.TransformToTree(x => x.ParentPath).Transform(x => new FtpEntry(x)).DisposeMany().Bind(out _tree).Subscribe();
+            var rootNode = new Tree("FTP Directory").Guide(TreeGuide.BoldLine).Style("green");
+            var treeNode = rootNode.AddNode("root");
+            foreach (var node in _tree)
+            {
+                AddToTree(treeNode, node);
+            }
+            AnsiConsole.Write(rootNode);
         }
         catch (Exception e)
         {
             Console.WriteLine(e);
             throw;
         }
-        
-        
+    }
+    private void AddToTree(TreeNode tree,  FtpEntry node)
+    {
+        var treeNode = tree.AddNode(node.Key);
+        foreach (var childrenItem in node.Items)
+        {
+            AddToTree(treeNode, childrenItem);
+        }
     }
 }
---
+++ src/Asv.Mavlink.Shell/Commands/FtpEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using DynamicData;
+
+namespace Asv.Mavlink.Shell;
+
+public class FtpEntry
+{
+    private readonly Node<IFtpEntry, string> _node;
+    private readonly ReadOnlyObservableCollection<FtpEntry> _items;
+    
+    public FtpEntry(Node<IFtpEntry, string> node)
+    {
+        _node = node;
+        _node.Children.Connect()
+            .Transform(childNode => new FtpEntry(childNode))
+            .Bind(out _items)
+            .Subscribe();
+        Key = node.Key;
+        Depth = node.Depth;
+        IsRoot = node.IsRoot;
+        Item = node.Item;
+    }
+
+    public string Key { get; set; }
+    public int Depth { get; set; }
+    public bool IsRoot { get; set; }
+    public IFtpEntry Item { get; set; }
+    public Node<IFtpEntry, string> Node => _node;
+    public ReadOnlyObservableCollection<FtpEntry> Items => _items;
+}
