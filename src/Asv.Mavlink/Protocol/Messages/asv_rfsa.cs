8bcb9c15a82abee29e417e417388efb783a957f1
Alexey <248997@gmail.com>
2024-09-27 10:41:21 +0500

    feat(rsga): add new modes

--- src/Asv.Mavlink/Protocol/Messages/asv_rfsa.cs
+++ src/Asv.Mavlink/Protocol/Messages/asv_rfsa.cs
@@ -1,6 +1,6 @@
 // MIT License
 //
-// Copyright (c) 2023 asv-soft (https://github.com/asv-soft)
+// Copyright (c) 2024 asv-soft (https://github.com/asv-soft)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,14 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-// This code was generate by tool Asv.Mavlink.Shell version 3.7.1+98c5c7a392002d9bb54507cd50df001a14c44120
+// This code was generate by tool Asv.Mavlink.Shell version 3.10.4+c1002429a625f2cf26c5bd2680700906e0b44d76
+
+using System;
+using System.Text;
+using System.ComponentModel;
+using Asv.Mavlink.V2.Common;
+using Asv.Mavlink.V2.Minimal;
+using Asv.IO;
 
 namespace Asv.Mavlink.V2.AsvRfsa
 {
