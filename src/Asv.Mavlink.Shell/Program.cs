e87168acc3407eadb685874d2ef8b7766c5dae2f
Maxim <u04@asv.me>
2024-09-27 14:08:23 +0300

    feat(shell): migrate mavlink shell to ConsoleAppFramework
    asana: https://app.asana.com/0/1203851531040615/1208405394310868/f

--- src/Asv.Mavlink.Shell/Program.cs
+++ src/Asv.Mavlink.Shell/Program.cs
@@ -1,31 +1,23 @@
 using System;
 using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 using Asv.IO;
-using ManyConsole;
+using ConsoleAppFramework;
 
-namespace Asv.Mavlink.Shell
-{
-    class Program
+namespace Asv.Mavlink.Shell;
+
+class Program
+{ 
+    static async Task Main(string[] args)
     {
-        static int Main(string[] args)
-        {
-            Console.InputEncoding = Encoding.UTF8;
-            Console.OutputEncoding = Encoding.UTF8;
-
-            typeof(Program).Assembly.PrintWelcomeToConsole();
-
-            try
-            {
-                var commands = ConsoleCommandDispatcher.FindCommandsInAssembly(Assembly.GetExecutingAssembly());
-                return ConsoleCommandDispatcher.DispatchCommand(commands, args, Console.Out);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(@"Unhandled exception: {0}", ex);
-                return -1;
-            }
-
-        }
+        Assembly.GetExecutingAssembly().PrintWelcomeToConsole();
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+        
+        var app = ConsoleApp.Create();
+        app.Add<ExampleCommand>();
+        await app.RunAsync(args);
     }
 }
+
