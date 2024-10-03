50b5e2f3330f6fb38eb8b3264d0ce897048305c1
Eugene <u03@asv.me>
2024-10-03 04:21:51 +0300

    feat(shell): add PacketViewerCommand with packet name filter
    Asana: https://app.asana.com/0/1206009821057985/1208405394310870/f

--- src/Asv.Mavlink.Shell/Commands/PacketViewerCommand.cs
+++ src/Asv.Mavlink.Shell/Commands/PacketViewerCommand.cs
@@ -2,13 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading;
 using System.Threading.Tasks;
-using Asv.Common;
-using Asv.IO;
-using Asv.Mavlink.V2.Common;
 using ConsoleAppFramework;
 using DynamicData;
 using DynamicData.Binding;
@@ -21,59 +18,112 @@ public class PacketViewerCommand
 {
     private Table _table;
     private IMavlinkRouter _router;
+    private Thread _actionsThread;
     private SourceList<PacketModel> PacketsSource { get; set; } = new();
     private ReadOnlyObservableCollection<PacketModel> _packets;
+    private ReadOnlyObservableCollection<PacketModel> _filterByName = new(new ObservableCollection<PacketModel>());
     private string consoleSearch;
+    private Subject<Func<PacketModel, bool>> _filterNameUpdate = new();
+    private bool _isFilterEnabled;
 
     [Command("packetviewer")]
     public void Run(string connection)
     {
-        consoleSearch = AnsiConsole.Prompt(new TextPrompt<string>("Type: "));
-        _table = new Table().Expand()
-            .AddColumns("Time", "Source", "Size", "Type", "Message")
-            .Title("[aqua]Packet Viewer[/]")
-            .Border(TableBorder.Rounded);
-        _router = new MavlinkRouter(MavlinkV2Connection.RegisterDefaultDialects);
-        _router.AddPort(new MavlinkPortConfig()
+        UpdateTable();
+        AnsiConsole.Status().Start("Create router...", ctx =>
         {
-            ConnectionString = connection,
-            IsEnabled = true,
-            Name = "Client"
+            ctx.Spinner(Spinner.Known.Aesthetic);
+            ctx.SpinnerStyle(Style.Parse("green"));
+            _router = new MavlinkRouter(MavlinkV2Connection.RegisterDefaultDialects);
+            _router.AddPort(new MavlinkPortConfig()
+            {
+                ConnectionString = connection,
+                IsEnabled = true,
+                Name = "Client"
+            });
+            _router.WrapToV2ExtensionEnabled = true;
         });
-        _router.WrapToV2ExtensionEnabled = true;
+        _filterNameUpdate.OnNext(FilterBySourceType);
         PacketsSource.LimitSizeTo(1000).Subscribe();
         PacketsSource.Connect().Sort(SortExpressionComparer<PacketModel>.Descending(_ => _.Time)).Bind(out _packets)
             .Subscribe(_ => OutputMessage(_packets.First()));
-        _router.Buffer(TimeSpan.FromSeconds(1)).Select(GetPacketAndAddToCollection).Subscribe(PacketsSource.AddRange);
-        AnsiConsole.Live(_table)
-            .Start(ctx => 
+        _router.Buffer(TimeSpan.FromSeconds(1)).Select(GetPacketAndAddToCollection)
+            .Subscribe(PacketsSource.AddRange);
+        while (true)
         {
-            while (true)
+            if (_isFilterEnabled is false)
             {
-                Task.Delay(1000);
-                ctx.Refresh();
+                _actionsThread = new Thread(InterceptConsoleActions);
+                _actionsThread.Start();
+                var ctx = AnsiConsole.Live(_table).AutoClear(true);
+                ctx.Start(WritePacketViewer);
             }
+            consoleSearch = AnsiConsole.Prompt(new TextPrompt<string>("Write Package Name:"));
+            UpdateTable();
           
-        });
-        _packets.WhenValueChanged(_ => _.Count).Subscribe(_ => { OutputMessage(_packets.Last()); }).Dispose();
+            _isFilterEnabled = false;
+            //AnsiConsole.Clear(); console freezes after cleaning it idk where is the problem
+        }
+    }
+
+    private void UpdateTable()
+    {
+        _table = new Table().Expand()
+            .AddColumns("Time", "Source", "Size", "Name", "Message")
+            .Title("[aqua]Packet Viewer[/]")
+            .Border(TableBorder.Rounded);
+    }
+
+    private void InterceptConsoleActions()
+    {
+        var actionsTable = new Table();
+        actionsTable.AddColumn("Filter");
+        actionsTable.AddRow(new Panel("[red]F[/]"));
+        AnsiConsole.Write(actionsTable);
+        var key = Console.ReadKey();
+        if (key.Key == ConsoleKey.F)
+        {
+            _isFilterEnabled = true;
+        }
+    }
+
+    private void WritePacketViewer(LiveDisplayContext context)
+    {
+        while (_isFilterEnabled is false)
+        {
+            Task.Delay(TimeSpan.FromSeconds(1));
+            if (_table is null) return;
+            context.Refresh();
+        }
+    }
+
+    private bool FilterBySourceType(PacketModel vm)
+    {
+        if (consoleSearch is null) return true;
+        return vm.Type.Contains(consoleSearch, StringComparison.InvariantCultureIgnoreCase);
     }
 
     private void OutputMessage(PacketModel packet)
     {
+        if (packet is null) return;
         _table.InsertRow(0, $"{packet.Time}", $"{packet.Source}", $"{packet.Size}", $"{packet.Type}",
             $"{packet.Message}");
         if (_table.Rows.Count == 20)
         {
             _table.RemoveRow(19);
         }
-       
     }
 
     private List<PacketModel> GetPacketAndAddToCollection(IList<IPacketV2<IPayload>> pkt)
     {
-        return consoleSearch == String.Empty ?
-              pkt.Select(item => new PacketModel(item)).ToList() 
-            : pkt.Where(_ => _.Name.Contains(consoleSearch)).Select(item => new PacketModel(item)).ToList();
+        if (consoleSearch is null ||
+            pkt.Where(_ => _.Name.Contains(consoleSearch, StringComparison.InvariantCultureIgnoreCase)) is null)
+        {
+            return pkt.Select(item => new PacketModel(item)).ToList();
+        }
+
+        return pkt.Where(_ => _.Name.Contains(consoleSearch, StringComparison.InvariantCultureIgnoreCase))
+            .Select(item => new PacketModel(item)).ToList();
     }
 
     private class PacketModel(IPacketV2<IPayload> packetV2)
