3b11bbf2524b9ba2410dbed929941742f7d29dd3
Eugene <u03@asv.me>
2024-10-04 13:26:53 +0300

    feat(mavlink): add StatusTextPacketHandler and DefaultPacketHandler, fix PacketPrinter logic
    Asana: https://app.asana.com/0/1206009821057985/1208426174517736/f

--- src/Asv.Mavlink/PacketPrinter/PacketPrinter.cs
+++ src/Asv.Mavlink/PacketPrinter/PacketPrinter.cs
@@ -1,20 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using Asv.Mavlink;
 
 namespace Asv.Mavlink;
 
-public class PacketPrinter(IEnumerable<IPacketPrinterHandler> packetPrinterHandlers)
+public class PacketPrinter
 {
+    private ImmutableList<IPacketPrinterHandler> _packetHandlers;
+    public PacketPrinter(IEnumerable<IPacketPrinterHandler> packetPrinterHandlers)
+    {
+        _packetHandlers = packetPrinterHandlers.OrderBy(_ => _.Order).ToImmutableList();
+    }
     public string Print(IPacketV2<IPayload> packet, PacketFormatting formatting = PacketFormatting.None)
     {
-        foreach (var handler in packetPrinterHandlers)
+        if (packet is null)
+            throw new NullReferenceException($"Cannot read object {packet}");
+        foreach (var handler in from handler in _packetHandlers let can = handler.CanPrint(packet) where can select handler)
         {
-           var can = handler.CanPrint(packet);
-           if (can)
-           {
-               return handler.Print(packet);
-           }
+            return handler.Print(packet, formatting);
         }
 
         throw new ArgumentOutOfRangeException($"Not found accesible handler for {packet.Name}");
