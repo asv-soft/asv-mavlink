31ba61e75b031ef32eacef9096dc47a9c1a40cea
Лев <u05@asv.me>
2024-10-08 17:42:05 +0300

    fix namespace in Program.cs

--- src/Asv.Mavlink.Shell/Program.cs
+++ src/Asv.Mavlink.Shell/Program.cs
@@ -7,30 +7,29 @@ using ConsoleAppFramework;
 
 namespace Asv.Mavlink.Shell;
 
-    class Program
-    { 
-        static async Task Main(string[] args)
-        {
-            Assembly.GetExecutingAssembly().PrintWelcomeToConsole();
-            Console.InputEncoding = Encoding.UTF8;
-            Console.OutputEncoding = Encoding.UTF8;
-            Console.BackgroundColor = ConsoleColor.Black;
+class Program
+{ 
+    static async Task Main(string[] args)
+    {
+        Assembly.GetExecutingAssembly().PrintWelcomeToConsole();
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.BackgroundColor = ConsoleColor.Black;
           
-            var app = ConsoleApp.Create();
-            app.Add<ExampleCommand>();
-            app.Add<FtpTreeDirectory>();
-            app.Add<FtpBrowserDirectory>();
-            app.Add<DevicesInfoCommand>();
-            app.Add<GenerateCommand>();
-            app.Add<VirtualAdsbCommand>();
-            app.Add<ExportSdrData>();
-            app.Add<MavlinkCommand>();
-            app.Add<MavProxy>();
-            app.Add<BenchmarkBinSerializationCommand>();
-            app.Add<BenchmarkSerializationPacket>();
-            app.Add<PacketViewerCommand>();
-            await app.RunAsync(args);
-        }
+        var app = ConsoleApp.Create();
+        app.Add<ExampleCommand>();
+        app.Add<FtpTreeDirectory>();
+        app.Add<FtpBrowserDirectory>();
+        app.Add<DevicesInfoCommand>();
+        app.Add<GenerateCommand>();
+        app.Add<VirtualAdsbCommand>();
+        app.Add<ExportSdrData>();
+        app.Add<MavlinkCommand>();
+        app.Add<MavProxy>();
+        app.Add<BenchmarkBinSerializationCommand>();
+        app.Add<BenchmarkSerializationPacket>();
+        app.Add<PacketViewerCommand>();
+        await app.RunAsync(args);
     }
 }
 
