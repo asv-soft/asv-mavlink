7416ce6b29f5f9efdb3ac867956087beaa3545e3
Лев <u05@asv.me>
2024-10-09 18:26:32 +0300

    add simple test for ListDirectory

--- src/Asv.Mavlink.Test/Microservices/Ftp/FtpServerEx.Tests.cs
+++ src/Asv.Mavlink.Test/Microservices/Ftp/FtpServerEx.Tests.cs
@@ -1,6 +1,8 @@
+using System.Buffers;
 using System.IO;
 using System.IO.Abstractions.TestingHelpers;
 using System.Reactive.Concurrency;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
@@ -52,7 +54,7 @@ public class FtpServerExTests
     {
         // Arrange
         var fileName = "test.txt";
-        var root = Path.Combine("D:", "file");
+        var root = Path.Combine("D:", "temp");
         var fileSystem = SetUpFileSystem(root);
         SetUpServer(out var server);
         var fileDir = fileSystem.Path.Combine(root, "file");
@@ -81,7 +83,7 @@ public class FtpServerExTests
     {
         // Arrange
         var fileName = "test.txt";
-        var root = Path.Combine("D:", "file");
+        var root = Path.Combine("D:", "temp");
         var fileSystem = SetUpFileSystem(root);
         SetUpServer(out var server);
         var fileDir = fileSystem.Path.Combine(root, "file");
@@ -99,6 +101,40 @@ public class FtpServerExTests
         // Assert
         Assert.Equal(0, result.Session);
         Assert.Equal(0u, result.Size);
+    }
+
+    #endregion
+
+    #region ListDirectory
+
+    [Fact]
+    public async Task ListDirectory_LittleAmountOfEntries_Success()
+    {
+        // Arrange
+        var fileName = "test.txt";
+        var root = Path.Combine("D:", "temp");
+        var fileSystem = SetUpFileSystem(root);
+        SetUpServer(out var server);
+        var fileDir = fileSystem.Path.Combine(root, "files");
+        var filePath = fileSystem.Path.Combine(fileDir, fileName);
+        var filePath2 = fileSystem.Path.Combine(fileDir, "test2.txt");
+        fileSystem.AddDirectory(Path.Combine(fileDir, "Folder1"));
+        fileSystem.AddDirectory(Path.Combine(fileDir, "Folder2"));
+        fileSystem.AddDirectory(Path.Combine(fileDir, "Folder3"));
+        fileSystem.AddFile(filePath, new MockFileData("Something"));
+        fileSystem.AddFile(filePath2, new MockFileData(string.Empty));
+        var cfg = new MavlinkFtpServerExConfig
+        {
+            RootDirectory = root,
+        };
+        var serverEx = new FtpServerEx(cfg, server, fileSystem);
+        using var memory = MemoryPool<char>.Shared.Rent();
+
+        // Act
+        var result = await serverEx.ListDirectory(fileDir, 0, memory.Memory, CancellationToken.None);
+        
+        // Assert
+        Assert.Equal(59, result);
     }
 
     #endregion
