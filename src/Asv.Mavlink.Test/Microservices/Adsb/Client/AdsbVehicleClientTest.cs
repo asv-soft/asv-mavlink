609c3fab18a3a94275dfb91826e30060fcb932d9
Maxim <u04@asv.me>
2024-11-07 17:19:27 +0300

    test(adsb): add tests for server, client and complex test
    asana: https://app.asana.com/0/1203851531040615/1208687174422745/f

--- src/Asv.Mavlink.Test/Microservices/Adsb/Client/AdsbVehicleClientTest.cs
+++ src/Asv.Mavlink.Test/Microservices/Adsb/Client/AdsbVehicleClientTest.cs
@@ -1,16 +1,49 @@
-using Asv.Mavlink;
+using System;
+using System.Threading;
 using JetBrains.Annotations;
 using Xunit;
+using Xunit.Abstractions;
 
-namespace Asv.Mavlink.Test.Adsb.Client;
+namespace Asv.Mavlink.Test;
 
 [TestSubject(typeof(AdsbVehicleClient))]
-public class AdsbVehicleClientTest
+public class AdsbVehicleClientTest : ClientTestBase<AdsbVehicleClient>, IDisposable
 {
+    private readonly CancellationTokenSource _cancellationTokenSource;
+
+    private readonly AdsbVehicleClientConfig _config = new()
+    {
+        TargetTimeoutMs = 10_000,
+        CheckOldDevicesMs = 3_000,
+    };
+
+    public AdsbVehicleClientTest(ITestOutputHelper output) : base(output)
+    {
+        _cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+    }
+
+    protected override AdsbVehicleClient CreateClient(MavlinkClientIdentity identity, CoreServices core) =>
+        new(identity, _config, core);
 
     [Fact]
-    public void METHOD()
+    public void Ctor_ThrowsExceptions_ArgIsNullFail()
     {
-        
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            var client = new AdsbVehicleClient(null, _config, Core);
+        });
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            var client = new AdsbVehicleClient(Identity, _config, null);
+        });
+        Assert.Throws<NullReferenceException>(() =>
+        {
+            var client = new AdsbVehicleClient(Identity, null, Core);
+        });
+    }
+
+    public void Dispose()
+    {
+        _cancellationTokenSource.Dispose();
     }
 }
--- src/Asv.Mavlink.Test/Microservices/Adsb/Client/AdsbVehicleTest.cs
+++ src/Asv.Mavlink.Test/Microservices/Adsb/Client/AdsbVehicleTest.cs
@@ -1,16 +1,21 @@
-using Asv.Mavlink;
+using System;
+using Asv.Mavlink.V2.Common;
 using JetBrains.Annotations;
 using Xunit;
 
-namespace Asv.Mavlink.Test.Adsb.Client;
+namespace Asv.Mavlink.Test;
 
 [TestSubject(typeof(AdsbVehicle))]
 public class AdsbVehicleTest
 {
 
     [Fact]
-    public void METHOD()
+    public void Ctor_ThrowsNullException_ArgIsNull()
     {
-        
+        AdsbVehiclePayload? payload = null;
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            var vehicle = new AdsbVehicle(payload, 123);
+        });
     }
 }
