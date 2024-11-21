8bd2eced3d37048bb5c6c88865b08cf9b1ae780a
Maxim <u04@asv.me>
2024-11-21 16:12:42 +0300

    test(ParamsExt): add server, client and complex tests
    asana: https://app.asana.com/0/1203851531040615/1208687175405357/f

--- src/Asv.Mavlink.Test/Microservices/ParamsExt/Client/ParamsExtClientTest.cs
+++ src/Asv.Mavlink.Test/Microservices/ParamsExt/Client/ParamsExtClientTest.cs
@@ -1,13 +1,17 @@
-using Asv.Mavlink;
+using System;
+using System.Threading;
 using JetBrains.Annotations;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Asv.Mavlink.Test.ParamsExt.Client;
+namespace Asv.Mavlink.Test;
 
 [TestSubject(typeof(ParamsExtClient))]
-public class ParamsExtClientTest(ITestOutputHelper log) : ClientTestBase<ParamsExtClient>(log)
+public class ParamsExtClientTest : ClientTestBase<ParamsExtClient>, IDisposable
 {
+    private readonly CancellationTokenSource _cancellationTokenSource;
+    private readonly ParamsExtClient _client;
+    
     private readonly ParamsExtClientConfig _config = new ParamsExtClientExConfig
     {
         ReadAttemptCount = 5,
@@ -15,4 +19,23 @@ public class ParamsExtClientTest(ITestOutputHelper log) : ClientTestBase<ParamsE
         ReadListTimeoutMs = 1000
     };
     protected override ParamsExtClient CreateClient(MavlinkClientIdentity identity, CoreServices core) => new(identity, _config, core);
+
+    public ParamsExtClientTest(ITestOutputHelper log) : base(log)
+    {
+        _client = Client;
+        _cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5), TimeProvider.System);
+    }
+
+    [Fact]
+    public void Constructor_Null_Throws()
+    {
+        Assert.Throws<ArgumentNullException>(() => new ParamsExtClient(null!, _config, Core));
+        Assert.Throws<ArgumentNullException>(() => new ParamsExtClient(Identity, null!, Core));
+        Assert.Throws<ArgumentNullException>(() => new ParamsExtClient(Identity, _config, null!));
+    }
+    
+    public void Dispose()
+    {
+        _cancellationTokenSource.Dispose();
+    }
 }
