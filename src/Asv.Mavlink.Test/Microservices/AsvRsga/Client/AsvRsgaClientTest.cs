d7911a244200c09d25f5c53c278f9585b32c2295
Maxim <u04@asv.me>
2024-12-02 17:57:17 +0300

    test(rsga): add tests for client and server. Add complex tests
    asana: https://app.asana.com/0/1203851531040615/1208687175405346/f

--- src/Asv.Mavlink.Test/Microservices/AsvRsga/Client/AsvRsgaClientTest.cs
+++ src/Asv.Mavlink.Test/Microservices/AsvRsga/Client/AsvRsgaClientTest.cs
@@ -1,10 +1,58 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Asv.Mavlink.Test;
 
 [TestSubject(typeof(AsvRsgaClient))]
-public class AsvRsgaClientTest(ITestOutputHelper log) : ClientTestBase<AsvRsgaClient>(log)
+public class AsvRsgaClientTest : ClientTestBase<AsvRsgaClient>, IDisposable
 {
+    private readonly CancellationTokenSource _cancellationTokenSource;
+    private readonly AsvRsgaClient _client;
+
+    public AsvRsgaClientTest(ITestOutputHelper log) : base(log)
+    {
+        _client = Client;
+        _cancellationTokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(20), TimeProvider.System);
+    }
+
     protected override AsvRsgaClient CreateClient(MavlinkClientIdentity identity, CoreServices core) => new(identity, core);
+    
+    [Fact]
+    public void Ctor_ThrowsExceptions_ArgIsNullFail()
+    {
+        Assert.Throws<ArgumentNullException>(() => new AsvRsgaClient(null!, Core));
+        Assert.Throws<ArgumentNullException>(() => new AsvRsgaClient(Identity, null!));
+    }
+    
+    [Fact]
+    public async Task ReadAllInfo_ShouldThrowTimeout_Exception()
+    {
+        // Arrange
+        var attempts = 5;
+        var timeout = 1000;
+        
+        // Act
+        var t1 = Assert.ThrowsAsync<TimeoutException>(async () =>
+        {
+            await Client.GetCompatibilities(_cancellationTokenSource.Token);
+        });
+
+        var t2 = Task.Factory.StartNew(() =>
+        {
+            Time.Advance(TimeSpan.FromMilliseconds((timeout * attempts) + 1));
+        });
+        
+        //Assert
+        await Task.WhenAll(t1, t2);
+        Assert.Equal(attempts, Link.Client.TxPackets);
+    }
+    
+    public void Dispose()
+    {
+        _cancellationTokenSource.Dispose();
+    }
 }
