739d3f20d5f35a4456713a70a8cbfc1ff02cda27
Daria N <smb_udk@vk.com>
2025-06-20 12:41:16 +0300

    feat: rewrite DevicesInfoCommand with R3
    Asana: https://app.asana.com/1/1200746044236722/task/1209315664743522

--- src/Asv.Mavlink.Shell/Commands/DevicesInfoCommand.cs
+++ src/Asv.Mavlink.Shell/Commands/DevicesInfoCommand.cs
@@ -29,18 +29,17 @@ public class DevicesInfoCommand
     /// </summary>
     /// <param name="connectionString">-cs, The address of the connection to the mavlink device</param>
     /// <param name="iterations">-i, States how many iterations should the program work through</param>
-    /// <param name="devicesTimeout">-dt, (in seconds) States the lifetime of a mavlink device that shows no Heartbeat</param>
     /// <param name="refreshRate">-r, (in ms) States how fast should the console be refreshed</param>
     /// <returns></returns>
     [Command("devices-info")]
-    public int Run(string connectionString, uint? iterations = null, uint devicesTimeout = 10, uint refreshRate = 3000)
+    public int Run(string connectionString, uint? iterations = null, uint refreshRate = 3000)
     {
-        RunAsync(connectionString, iterations, devicesTimeout, refreshRate).Wait();
+        RunAsync(connectionString, iterations, refreshRate).Wait();
         ConsoleAppHelper.WaitCancelPressOrProcessExit();
         return 0;
     }
     
-private async Task RunAsync(string connectionString, uint? iterations, uint devicesTimeout,  uint refreshRate)
+private async Task RunAsync(string connectionString, uint? iterations,  uint refreshRate)
 {
     _refreshRate = refreshRate;
     var protocol = Protocol.Create(builder =>
@@ -102,68 +101,69 @@ private async Task RunAsync(string connectionString, uint? iterations, uint devi
         
         removeSub.Dispose();
         _list.Dispose();
-        
+
         await _explorer.DisposeAsync();
-        await _router.DisposeAsync();
+        await _router.DisposeAsync();    
     }
 }
 
-    private void RenderRows(Table table, ISynchronizedView<KeyValuePair<DeviceId,IClientDevice>,MavlinkDeviceModel> devices)
+    private void RenderRows(Table table, ISynchronizedView<KeyValuePair<DeviceId, IClientDevice>, MavlinkDeviceModel> devices)
     {
         foreach (var device in devices.Unfiltered)
         {
-            if (!device.View.IsInitialized)
+            if (device.View.State != ClientDeviceState.Complete)
             {
                 continue;
             }
 
             table.AddRow(
-                $"[red]{Markup.Escape(device.View.DeviceFullId.ToString())}[/]",
-                $"{Markup.Escape(device.View.Type)}",
+                $"[red]{Markup.Escape(device.View.DeviceFullId?.ToString() ?? "")}[/]",
+                $"{Markup.Escape(device.View.Type ?? "")}",
                 $"{Markup.Escape(device.View.SystemId.ToString())}",
                 $"{Markup.Escape(device.View.ComponentId.ToString())}",
                 $"{Markup.Escape(device.View.MavlinkVersion.ToString())}",
-                $"{Markup.Escape(device.View.BaseModeText)}",
-                $"{Markup.Escape(device.View.SystemStatusText)}",
-                $"[green]{Markup.Escape(device.View.RateText)}[/]"
+                $"{Markup.Escape(device.View.BaseModeText ?? "")}",
+                $"{Markup.Escape(device.View.SystemStatusText ?? "")}",
+                $"[green]{Markup.Escape(device.View.RateText ?? "")}[/]"
             );
         }
     }
 
+
     private class MavlinkDeviceModel: IDisposable
     {
+        private readonly CompositeDisposable _disposables = new();
+        private readonly IClientDevice _info;
         private uint _rate;
         private long _lastUpdate;
         private uint _lastRate;
         private bool _isInitialized;
-        private readonly CompositeDisposable _disposables = new();
-        private bool _disposed;
+        private bool _disposed; 
         
-        public bool IsInitialized => _isInitialized;
+        public ClientDeviceState State => _info.State.CurrentValue;
 
 
 #pragma warning disable CS8618, CS9264
         public MavlinkDeviceModel(IClientDevice info,TimeProvider timeProvider ) 
 #pragma warning restore CS8618, CS9264
        {
-
-           _disposables.Add(info.State
+           _info = info;
+           
+           _disposables.Add(_info.State
                .Where(state => state == ClientDeviceState.Complete)
                .Take(1)
                .Subscribe(_ =>
                {
-                   if (_isInitialized)
-                   {
-                       return;
-                   }
+                   if (_disposed) return;
+                   if (_isInitialized) return;
                    
                    _isInitialized = true;
 
-                   var heartbeat = info.GetMicroservice<IHeartbeatClient>() 
+                   var heartbeat = _info.GetMicroservice<IHeartbeatClient>() 
                                    ?? throw new Exception("Heartbeat client not initialized");
 
                    DeviceFullId = heartbeat.Identity.Target;
-                   Type = info.Id.DeviceClass;
+                   Type = _info.Id.DeviceClass;
                    SystemId = heartbeat.Identity.Target.SystemId;
                    ComponentId = heartbeat.Identity.Target.ComponentId;
 
@@ -178,6 +178,7 @@ private async Task RunAsync(string connectionString, uint? iterations, uint devi
                        .Timer(TimeSpan.FromSeconds(3), TimeSpan.FromSeconds(3), timeProvider)
                        .Subscribe(_ =>
                        {
+                           if (_disposed) return;
                            var time = Interlocked.Exchange(ref _lastUpdate, timeProvider.GetTimestamp());
                            var cnt = Interlocked.Exchange(ref _lastRate, 0);
                            var rate = cnt / timeProvider.GetElapsedTime(time).TotalSeconds;
@@ -188,18 +189,24 @@ private async Task RunAsync(string connectionString, uint? iterations, uint devi
                        .ThrottleLast(TimeSpan.FromSeconds(2), timeProvider)
                        .Subscribe(_ =>
                        {
+                           if (_disposed) return;
                            Interlocked.Increment(ref _lastRate);
                            ToggleLinkPing = false;
                            ToggleLinkPing = true;
                        }));
 
                    _disposables.Add(heartbeat.RawHeartbeat
-                       .Subscribe(_ => Interlocked.Increment(ref _rate)));
+                       .Subscribe(_ =>
+                       {
+                           if (_disposed) return;
+                           Interlocked.Increment(ref _rate);
+                       }));
 
                    _disposables.Add(heartbeat.RawHeartbeat
                        .WhereNotNull()
                        .Subscribe(p =>
                        {
+                           if (_disposed) return;
                            BaseModeText = $"{p.BaseMode.ToString("F").Replace("MavModeFlag", "")}";
                        }));
 
@@ -207,6 +214,7 @@ private async Task RunAsync(string connectionString, uint? iterations, uint devi
                        .WhereNotNull()
                        .Subscribe(p =>
                        {
+                           if (_disposed) return;
                            SystemStatusText = p.SystemStatus.ToString("G").Replace("MavState", "");
                        }));
                }));
