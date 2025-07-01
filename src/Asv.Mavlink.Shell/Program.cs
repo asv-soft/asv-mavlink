3bfdee78faad778ae9fb16ef7fb107e099b8be7c
Daria N <smb_udk@vk.com>
2025-07-01 13:33:40 +0300

    feat: corrections of mistakes after the review. Rewrite with SubscribeAwait
    asana: https://app.asana.com/1/1200746044236722/project/1203851531040615/task/1209315664743524?focus=true

--- src/Asv.Mavlink.Shell/Program.cs
+++ src/Asv.Mavlink.Shell/Program.cs
@@ -19,7 +19,7 @@ class Program
         // app.Add<FtpTreeDirectory>();
         // app.Add<FtpBrowserDirectory>();
         // app.Add<DevicesInfoCommand>();
-        // app.Add<VirtualAdsbCommand>();
+        app.Add<VirtualAdsbCommand>();
         // app.Add<ExportSdrData>();
         // app.Add<MavProxy>();
         // app.Add<BenchmarkBinSerializationCommand>();
