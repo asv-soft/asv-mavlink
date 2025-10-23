9900e6ad4c208a53da6977b262d11e6171cebfd1
tuxtender <tuxtender@gmail.com>
2025-10-23 22:26:53 +0500

    Expose motor test microservice in CLI

--- src/Asv.Mavlink.Shell/Program.cs
+++ src/Asv.Mavlink.Shell/Program.cs
@@ -33,6 +33,7 @@ class Program
         app.Add<ParamsCommand>();
         app.Add<PrintVehicleStateCommand>();
         app.Add<DownloadMissionItemsCommand>();
+        app.Add<MototTestCommand>();
         
         await app.RunAsync(args);
     }
