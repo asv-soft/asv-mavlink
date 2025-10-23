4f6c714a78e72d741f00ee3f73733dd172ddd2f9
tuxtender <tuxtender@gmail.com>
2025-10-23 12:55:43 +0500

    feat (asv-mavlink): add ArduCopter motor test microservice
    Test motors in order number, get telemetry for AP_MotorsMatrix vehicle configurations with fallback
    
    Asana: https://app.asana.com/1/1200746044236722/task/1211370345690971?focus=true

---
+++ src/Asv.Mavlink/Microservices/MotorTest/Client/ArduCopterMotorsMatrixConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Asv.Mavlink;
+
+internal record ArduCopterMotorsMatrixConfig(string Version, IEnumerable<Layout> Layouts);
+
+internal record Layout(int Class, int Type, IEnumerable<Motor> Motors);
+
+internal record Motor
+{
+	public int Number { get; init; }
+	public int TestOrder { get; init; }
+	public required string Rotation { get; init; }
+	public float Roll { get; init; }
+	public float Pitch { get; init; }
+}
---
+++ src/Asv.Mavlink/Microservices/MotorTest/Client/MotorPwm.cs
@@ -0,0 +1,8 @@
+﻿namespace Asv.Mavlink;
+
+public class MotorPwm
+{
+	public int? Id { get; init; }
+	public int Servo { get; init; }
+	public int Pwm { get; init; }
+}
