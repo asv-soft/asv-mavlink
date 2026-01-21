30f3445f24497a3316446daa0fcfed59a12ef958
shinoxzu <shinoxzu@gmail.com>
2026-01-22 00:04:14 +0300

    test: fix params microservice tests
    Make params microservice tests better.
    
    Asana: https://app.asana.com/1/1200746044236722/project/1203851531040615/task/1212897326874246?focus=true

--- src/Asv.Mavlink.Test/Microservices/Params/Client/Ex/ParamsClientExTest.cs
+++ src/Asv.Mavlink.Test/Microservices/Params/Client/Ex/ParamsClientExTest.cs
@@ -1,151 +1,117 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Asv.Mavlink.Common;
 using JetBrains.Annotations;
+using R3;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Asv.Mavlink.Test;
 
 [TestSubject(typeof(ParamsClientEx))]
-public class ParamsClientExTest : ClientTestBase<ParamsClientEx>, IDisposable
+public class ParamsClientExTest : ClientTestBase<ParamsClientEx>
 {
     private readonly CancellationTokenSource _cancellationTokenSource;
-    private List<ParamDescription> _existDescription;
-    private readonly MavParamCStyleEncoding _encoding;
-    private ParamsClient _client;
-
     private readonly ParamsClientExConfig _config = new()
     {
         ReadTimeouMs = 100,
         ReadAttemptCount = 3,
         ChunkUpdateBufferMs = 100,
     };
-
-#pragma warning disable CS8618, CS9264
+    
     public ParamsClientExTest(ITestOutputHelper log) : base(log)
-#pragma warning restore CS8618, CS9264
     {
-        _encoding = new MavParamCStyleEncoding();
         _cancellationTokenSource = new CancellationTokenSource();
     }
 
+    [Fact]
+    public void Constructor_NullArguments_Throws()
+    {
+        Assert.Throws<ArgumentNullException>(
+            () => new ParamsClientEx(null!, _config, ParamsTestHelper.Encoding, ParamsTestHelper.ClientParamsDescriptions));
+        Assert.Throws<ArgumentNullException>(
+            () => new ParamsClientEx(Client.Base, null!, ParamsTestHelper.Encoding, ParamsTestHelper.ClientParamsDescriptions));
+        Assert.Throws<ArgumentNullException>(
+            () => new ParamsClientEx(Client.Base, _config, null!, ParamsTestHelper.ClientParamsDescriptions));
+        Assert.Throws<ArgumentNullException>(
+            () => new ParamsClientEx(Client.Base, _config, ParamsTestHelper.Encoding, null!));
+    }
+    
+    [Fact]
+    public async Task ReadOnce_Timeout_Throws()
+    {
+        // Arrange
+        var paramName = ParamsTestHelper.ClientParamsDescriptions.First().Name;
+        
+        var called = 0;
+        using var s1 = Link.Client.OnTxMessage.Synchronize().Subscribe(_ =>
+        {
+            called++;
+            Time.Advance(TimeSpan.FromMilliseconds(_config.ReadTimeouMs + 1));
+        });
+        
+        // Act
+        var task = Client.ReadOnce(paramName, _cancellationTokenSource.Token);
+        
+        // Assert
+        await Assert.ThrowsAsync<TimeoutException>(async () => await task);
+        Assert.Equal(_config.ReadAttemptCount, called);
+        Assert.Equal(0, (int)Link.Server.Statistic.TxMessages);
+        Assert.Equal(0, (int)Link.Client.Statistic.RxMessages);
+    }
+    
+    [Fact]
+    public async Task ReadAll_Timeout_Throws()
+    {
+        // Arrange
+        var called = 0;
+        using var s1 = Link.Client.OnTxMessage.Synchronize().Subscribe(_ =>
+        {
+            called++;
+            Time.Advance(TimeSpan.FromMilliseconds(_config.ReadTimeouMs * (_config.ReadAttemptCount + 1) + 1));
+        });
+        
+        // Act
+        await Client.ReadAll(null, false, _cancellationTokenSource.Token);
+        
+        // Assert
+        Assert.Equal(1, called);
+        Assert.Equal(0, (int)Link.Server.Statistic.TxMessages);
+        Assert.Equal(0, (int)Link.Client.Statistic.RxMessages);
+        Assert.Equal(0, Client.LocalCount.CurrentValue);
+        Assert.Empty(Client.Items);
+    }
+    
+    [Fact]
+    public async Task WriteOnce_Timeout_Throws()
+    {
+        // Arrange
+        var paramName = ParamsTestHelper.ServerParamsMeta.First().Name;
+        var paramValue = ParamsTestHelper.ServerParamsMeta.First().DefaultValue;
+        
+        var called = 0;
+        using var s1 = Link.Client.OnTxMessage.Synchronize().Subscribe(_ =>
+        {
+            called++;
+            Time.Advance(TimeSpan.FromMilliseconds(_config.ReadTimeouMs + 1));
+        });
+        
+        // Act
+        var task = Client.WriteOnce(paramName, paramValue, _cancellationTokenSource.Token);
+
+        // Assert
+        await Assert.ThrowsAsync<TimeoutException>(async () => await task);
+        Assert.Equal(_config.ReadAttemptCount, called);
+        Assert.Equal(0, (int)Link.Server.Statistic.TxMessages);
+        Assert.Equal(0, (int)Link.Client.Statistic.RxMessages);
+        Assert.Equal(_config.ReadAttemptCount, (int)Link.Client.Statistic.TxMessages);
+    }
+    
     protected override ParamsClientEx CreateClient(MavlinkClientIdentity identity, CoreServices core)
     {
-        _client = new ParamsClient(identity, _config, core);
-        _existDescription = new List<ParamDescription>
-        {
-            new() { Name = "BARO_PRIMARY", ParamType = MavParamType.MavParamTypeUint8 },
-            new() { Name = "SIM_CAN_TYPE1", ParamType = MavParamType.MavParamTypeInt8 },
-            new() { Name = "FENCE_ACTION", ParamType = MavParamType.MavParamTypeUint16 },
-            new() { Name = "RC7_DZ", ParamType = MavParamType.MavParamTypeInt16 },
-            new() { Name = "LOG_BITMASK", ParamType = MavParamType.MavParamTypeUint32 },
-            new() { Name = "WPNAV_SPEED", ParamType = MavParamType.MavParamTypeInt32 },
-            new() { Name = "BARO_ALT_OFFSET", ParamType = MavParamType.MavParamTypeReal32 },
-        };
-        return new ParamsClientEx(_client, _config, _encoding, _existDescription);
-    }
-
-    [Fact]
-    public void Constructor_Null_Throws()
-    {
-        // Act
-        // ReSharper disable once NullableWarningSuppressionIsUsed
-        Assert.Throws<NullReferenceException>(() => new ParamsClientEx(null!, _config, _encoding, _existDescription));
-        // ReSharper disable once NullableWarningSuppressionIsUsed
-        Assert.Throws<NullReferenceException>(() => new ParamsClientEx(_client, null!, _encoding, _existDescription));
-        // ReSharper disable once NullableWarningSuppressionIsUsed
-        Assert.Throws<NullReferenceException>(() => new ParamsClientEx(_client, _config, null!, _existDescription));
-        // ReSharper disable once NullableWarningSuppressionIsUsed
-        Assert.Throws<NullReferenceException>(() => new ParamsClientEx(_client, _config, _encoding, null!));
-    }
-    
-    [Fact]
-    public async Task ReadOnce_ShouldThrowTimeout_Exception()
-    {
-        // Act
-        var t1 = Assert.ThrowsAsync<TimeoutException>(async () =>
-        {
-            await Client.ReadOnce("BARO_PRIMARY", _cancellationTokenSource.Token);
-        });
-
-        var t2 = Task.Factory.StartNew(() =>
-        {
-            while (t1.IsCompleted == false)
-            {
-                Time.Advance(TimeSpan.FromMilliseconds(_config.ReadTimeouMs * _config.ReadAttemptCount + 1));
-            }
-        });
-        
-        //Assert
-        await Task.WhenAll(t1, t2);
-        Assert.Equal(_config.ReadAttemptCount, (int)Link.Client.Statistic.TxMessages);
-    }
-    
-    [Fact(Skip = "Test is not relevant")]
-    public async Task ReadAll_ShouldThrowTimeout_Exception()
-    {
-        // Act
-        var t1 = Assert.ThrowsAsync<TaskCanceledException>(async () =>
-        {
-            await Client.ReadAll(null, false, _cancellationTokenSource.Token);
-        });
-
-        var t2 = Task.Factory.StartNew(() =>
-        {
-            Time.Advance(TimeSpan.FromMilliseconds(_config.ReadTimeouMs * _config.ReadAttemptCount + 1));
-        });
-        
-        //Assert
-        await Task.WhenAll(t1, t2);
-        Assert.Equal(_config.ReadAttemptCount, (int)Link.Client.Statistic.TxMessages); 
-    }
-    
-    [Fact]
-    public async Task WriteOnce_ShouldThrowTimeout_Exception()
-    {
-        // Arrange
-        var value = new MavParamValue(2000);
-        
-        // Act
-        var t1 = Assert.ThrowsAsync<TimeoutException>(async () =>
-        {
-            await Client.WriteOnce("BARO_PRIMARY", value, _cancellationTokenSource.Token);
-        });
-
-        var t2 = Task.Factory.StartNew(() =>
-        {
-            Time.Advance(TimeSpan.FromMilliseconds(_config.ReadTimeouMs * _config.ReadAttemptCount + 1));
-        });
-
-        //Assert
-        await Task.WhenAll(t1, t2);
-        Assert.Equal(_config.ReadAttemptCount, (int)Link.Client.Statistic.TxMessages);
-    }
-    
-    [Fact]
-    public async Task Read_PacketSend_ThrowWhenTimeout()
-    {
-        // Arrange
-        var name = "BARO_PRIMARY";
-        
-        // Act
-        var task = Assert.ThrowsAsync<TimeoutException>(async () =>
-        {
-            await Client.ReadOnce(name, _cancellationTokenSource.Token);
-        });
-        
-        var taskTime = Task.Factory.StartNew(() =>
-        {
-            Time.Advance(TimeSpan.FromMilliseconds(_config.ReadTimeouMs * _config.ReadAttemptCount + 1));
-        });
-        
-        //Assert
-        await Task.WhenAll(task, taskTime);
-        Assert.Equal(_config.ReadAttemptCount, (int)Link.Client.Statistic.TxMessages);
-
+        var paramsClient = new ParamsClient(identity, _config, core);
+        return new ParamsClientEx(paramsClient, _config, ParamsTestHelper.Encoding, ParamsTestHelper.ClientParamsDescriptions);
     }
     
     protected override void Dispose(bool disposing)
